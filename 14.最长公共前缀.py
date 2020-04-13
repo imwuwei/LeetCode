d22945dee6839748452d9197f6962eefc35d79a9
joywuwei <1762615168@qq.com>
2020-04-13 20:46:49 +0800

    modified:   14.最长公共前缀.py 	new file:   20.有效的括号.py

--- 14.最长公共前缀.py
+++ 14.最长公共前缀.py
@@ -7,10 +7,15 @@
 # @lc code=start
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        import os
-        return os.path.commonprefix(strs)
-
-
+        # import os
+        # return os.path.commonprefix(strs)
+        if len(strs) == 0:
+            return ""
+        s = strs[0]
+        for i in range(1, len(strs)):
+            while strs[i].find(s) !=0:
+                s = s[:-1]
+        return s
     
 # @lc code=end
 
